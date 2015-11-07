446cffed71010c326c65d39ab6f4973a8cd1ec80
IronBeard <GarethPhilpott@hotmail.com>
2015-11-08 12:06:51 +1300

    Changed MOUNTOPTIONS for better performance
    Change sync to async in MOUNTOPTIONS to reduce wear on the drives and to
    increase speed for copying.
    
    It is not part of my media server design for the the drives that I am
    using to be unplugged.

--- InstallUSBMount.sh
+++ InstallUSBMount.sh
@@ -7,3 +7,6 @@ cp /etc/usbmount/usbmount.conf /etc/usbmount/usbmount.conf.bak
 # Alter the usbmount.conf to include NTFS
 sed -i 's|FILESYSTEMS="vfat ext2 ext3 ext4 hfsplus"|FILESYSTEMS="vfat ext2 ext3 ext4 hfsplus ntfs"|g' /etc/usbmount/usbmount.conf
 sed -i 's|FS_MOUNTOPTIONS=""|FS_MOUNTOPTIONS="-fstype=ntfs,gid=users,umask=0 -fstype=vfat,gid=users,umask=0 -fstype=ext4,gid=users,umask=0"|g' /etc/usbmount/usbmount.conf
+
+# Alter the usbmount.conf to change the sync to async in MOUNTOPTIONS
+sed -i 's|MOUNTOPTIONS="sync,noexec,nodev,noatime,nodiratime"|MOUNTOPTIONS="async,noexec,nodev,noatime,nodiratime"|g' /etc/usbmount/usbmount.conf
