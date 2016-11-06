4f5654ab38131769307470a32cd5aedb282ebb83
Ironbeard <GarethPhilpott@hotmail.com>
2016-11-06 20:50:52 +1300

    A script to restart the Network when needed
    Initial version, needs testing.

---
+++ restartNetwork.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+#Functions
+function ScreenLines {
+  printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+}
+
+#Text Colors
+NONE='\033[00m'
+BLACK='\033[00;30m'
+BLUE='\033[00;34m'
+GREEN='\033[00;32m'
+CYAN='\033[00;36m'
+RED='\033[00;31m'
+PURPLE='\033[00;35m'
+BROWN='\033[00;33m'
+LIGHTGRAY='\033[00;37m'
+DARKGRAY='\033[01;30m'
+LIGHTBLUE='\033[01;34m'
+LIGHTGREEN='\033[01;32m'
+LIGHTCYAN='\033[01;36m'
+LIGHTRED='\033[01;31m'
+LIGHTPURPLE='\033[01;35m'
+YELLOW='\033[01;33m'
+WHITE='\033[01;37m'
+BOLD='\033[1m'
+UNDERLINE='\033[4m'
+
+echo -e ${WHITE}`date`$(tput sgr0)
+
+#Ensure that the script has been run as root
+if [ `whoami` != root ]; then
+  echo -e ${WHITE}"Due to the nature of the changes that this script makes please run it as ${RED}root ${WHITE}or by using ${RED}sudo"$(tput sgr0)
+  exit
+fi
+
+echo -e ${WHITE}"ifdown eth0"$(tput sgr0)
+ifdown eth0
+
+echo -e ${WHITE}"ifup eth0"$(tput sgr0)
+ifup eth0
+
+echo -e ${WHITE}"Restart Complete"$(tput sgr0)
